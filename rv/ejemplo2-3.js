95997da88074d66dd954494edcbeb22ec232c879
ferchapag <fernando.chapa.g5@gmail.com>
2016-08-16 15:20:10 -0500

    Update ejemplo2-3.js

--- rv/ejemplo2-3.js
+++ rv/ejemplo2-3.js
@@ -1,5 +1,5 @@
   var miCanvas = document.getElementById("miCanvas");
-  var contextp = miCanvas.getContext("2d");
+  var contexto = miCanvas.getContext("2d");
   contexto.fillStyle ="red";
   contexto.fillRect(30,30,50,50);
   
