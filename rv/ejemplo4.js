4c7320d98b8fe0218df56996643ad9eb2fcba08d
ferchapag <fernando.chapa.g5@gmail.com>
2016-08-18 15:27:31 -0500

    Update ejemplo4.js

--- rv/ejemplo4.js
+++ rv/ejemplo4.js
@@ -1,1 +1,16 @@
 var escena=new THREE.Scene();
+var camara=new THREE.PerspectiveCamera();
+camara.position.z=5;
+
+var renderizador= new THREE.WebGLRender();
+renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
+
+document.body.appendChild(renderizador.domElement);
+var forma=new THREE.BoxGeometry(1,1,1);
+var material =new THREE.MeshNormalMaterial();
+var cubo=new THREE.MEsh(forma, material);
+
+cubo.rotateX(-Math.PI/4);
+cubo.rotateY(-Math.PI/4);
+
+renderizador.render(escena,camara);
