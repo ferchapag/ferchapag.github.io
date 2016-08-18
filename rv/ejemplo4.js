4641a59e87fadc2c72607cea44439232ec5d075c
ferchapag <fernando.chapa.g5@gmail.com>
2016-08-18 15:38:17 -0500

    Update ejemplo4.js

--- rv/ejemplo4.js
+++ rv/ejemplo4.js
@@ -8,7 +8,7 @@ renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 var forma=new THREE.BoxGeometry(1,1,1);
 var material =new THREE.MeshNormalMaterial();
-var cubo=new THREE.MEsh(forma, material);
+var cubo=new THREE.Mesh(forma, material);
 
 cubo.rotateX(-Math.PI/4);
 cubo.rotateY(-Math.PI/4);
