36a73d931148e1c72a0f6183132b301d25bee033
ferchapag <fernando.chapa.g5@gmail.com>
2016-08-18 15:37:16 -0500

    Update ejemplo4.js

--- rv/ejemplo4.js
+++ rv/ejemplo4.js
@@ -2,7 +2,7 @@ var escena=new THREE.Scene();
 var camara=new THREE.PerspectiveCamera();
 camara.position.z=5;
 
-var renderizador= new THREE.WebGLRender();
+var renderizador= new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 
 document.body.appendChild(renderizador.domElement);
