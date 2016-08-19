900a573cdfa3d840bd6ed9bfc04251da4c74a592
ferchapag <fernando.chapa.g5@gmail.com>
2016-08-18 23:45:59 -0500

    Update ejercicio2arbol.js

--- rv/ejercicio2arbol.js
+++ rv/ejercicio2arbol.js
@@ -1,6 +1,7 @@
 var escena=new THREE.Scene();
 var camara=new THREE.PerspectiveCamera();
-camara.position.z=5;
+camara.position.z=100;
+camara.position.y=100;
 
 var renderizador= new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
