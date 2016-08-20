38251ad965977d806772021907390d53778c02f5
ferchapag <fernando.chapa.g5@gmail.com>
2016-08-19 23:31:34 -0500

    Update ejercicio2arbol.js

--- rv/ejercicio2arbol.js
+++ rv/ejercicio2arbol.js
@@ -7,16 +7,15 @@ var renderizador= new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
 
 document.body.appendChild(renderizador.domElement);
-var forma=new THREE.CylinderGeometry(5,5,50,60);
+var forma=new THREE.CylinderGeometry(30,22,50,60);
 var material =new THREE.MeshNormalMaterial("brown");
 var material2 =new THREE.MeshNormalMaterial("green");
 var tronco=new THREE.Mesh(forma, material);
 
 
-var forma2=new THREE.SphereGeometry(20,50,60);
+var forma2=new THREE.SphereGeometry(20,60,60);
 var hojas=new THREE.Mesh(forma2,material2);
-hojas.position.y=50;
-hojas.fillStyle="green";
+hojas.position.y=40;
 
 escena.add(tronco);
 escena.add(hojas);
