4dc450f929efc3ef2f124ae8a2a40ca1f66fadf1
ferchapag <fernando.chapa.g5@gmail.com>
2016-08-23 15:54:38 -0500

    Update cubo.js

--- rv/cubo.js
+++ rv/cubo.js
@@ -9,18 +9,18 @@ forma.vertices.push( new THREE.Vector3(1,1,-1));//5
 forma.vertices.push( new THREE.Vector3(-1,1,-1));
 forma.vertices.push( new THREE.Vector3(-1,1,1));//7
 
-forma.faces.push(new THREE.Face3(0,2,1));//base
-forma.faces.push(new THREE.Face3(2,3,0));
-forma.faces.push(new THREE.Face3(6,5,4));//arriba
-forma.faces.push(new THREE.Face3(6,7,4));
-forma.faces.push(new THREE.Face3(3,0,4));//enfrente
-forma.faces.push(new THREE.Face3(4,7,3));
-forma.faces.push(new THREE.Face3(3,2,7));//izq
-forma.faces.push(new THREE.Face3(6,7,2));
-forma.faces.push(new THREE.Face3(6,5,2));//atras
-forma.faces.push(new THREE.Face3(2,5,1));
+forma.faces.push(new THREE.Face3(3,2,1));//base
+forma.faces.push(new THREE.Face3(3,1,0));
+forma.faces.push(new THREE.Face3(7,6,5));//arriba
+forma.faces.push(new THREE.Face3(7,5,4));
+forma.faces.push(new THREE.Face3(3,7,4));//enfrente
+forma.faces.push(new THREE.Face3(3,4,0));
+forma.faces.push(new THREE.Face3(2,3,7));//izq
+forma.faces.push(new THREE.Face3(2,6,7));
+forma.faces.push(new THREE.Face3(2,1,5));//atras
+forma.faces.push(new THREE.Face3(2,6,5));
 forma.faces.push(new THREE.Face3(0,1,5));//derecha
-forma.faces.push(new THREE.Face3(0,5,4));
+forma.faces.push(new THREE.Face3(0,4,5));
 
 forma.computeBoundingSphere();
 forma.computeFaceNormals();
