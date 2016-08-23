52c0dfdb3f40181cfb1e2b33e1ca10f7863c9648
ferchapag <fernando.chapa.g5@gmail.com>
2016-08-23 15:41:24 -0500

    Create cubo.js

---
+++ rv/cubo.js
@@ -0,0 +1,42 @@
+var forma=new THREE.Geometry();
+
+forma.vertices.push( new THREE.Vector3(1,0,1));//0
+forma.vertices.push( new THREE.Vector3(1,0,-1));//1
+forma.vertices.push( new THREE.Vector3(-1,0,-1));
+forma.vertices.push( new THREE.Vector3(-1,0,1));//3
+forma.vertices.push( new THREE.Vector3(1,1,1));
+forma.vertices.push( new THREE.Vector3(1,1,-1));//5
+forma.vertices.push( new THREE.Vector3(-1,1,-1));
+forma.vertices.push( new THREE.Vector3(-1,1,1));//7
+
+forma.faces.push(new THREE.Face3(0,2,1));//une a los vertices  3 con 2 y uno, se forma un triángulo
+forma.faces.push(new THREE.Face3(2,3,0));
+forma.faces.push(new THREE.Face3(6,5,4));
+forma.faces.push(new THREE.Face3(6,7,4));
+forma.faces.push(new THREE.Face3(3,0,4));
+forma.faces.push(new THREE.Face3(0,7,3));
+forma.faces.push(new THREE.Face3(3,2,7));
+forma.faces.push(new THREE.Face3(6,7,3));
+forma.faces.push(new THREE.Face3(6,5,2));
+forma.faces.push(new THREE.Face3(6,5,1));
+forma.faces.push(new THREE.Face3(0,1,5));
+forma.faces.push(new THREE.Face3(0,1,4));
+
+forma.computeBoundingSphere();
+forma.computeFaceNormals();
+
+var material=new THREE.MeshNormalMaterial();
+var malla= new THREE.Mesh(forma, material);
+malla.rotateX(Math.PI/4);
+
+var escena=new THREE.Scene();
+escena.add(malla);
+
+var camara= new THREE.PerspectiveCamera();
+camara.position.z=5;
+
+var renderizador= new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95,
+                      window.innerHeight*.95);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena, camara);
