8abc8c019861d7242ac4a43fe5886f4d4e68c0cb
ferchapag <fernando.chapa.g5@gmail.com>
2016-08-25 15:05:55 -0500

    Update ejemplo-figura.js

--- rv/ejemplo-figura.js
+++ rv/ejemplo-figura.js
@@ -2,7 +2,7 @@ var figura= new THREE.Shape();
 
 figura.moveTo(10,10);
 figura.lineTo(10,40);
-figura.lineTo(40,10);
+figura.lineTo(40,40);
 figura.lineTo(10,10);
 
 var forma=new THREE.ShapeGeometry(figura);
