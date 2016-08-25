0bea843180ad1bf92ce66855feb4855b02d91506
ferchapag <fernando.chapa.g5@gmail.com>
2016-08-25 15:37:36 -0500

    Update ejemplo-extrusion.js

--- rv/ejemplo-extrusion.js
+++ rv/ejemplo-extrusion.js
@@ -16,6 +16,7 @@ escena.add(malla);
 var camara=new THREE.PerspectiveCamera();
 camara.position.z=500;
 var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
+renderizador.setSize(window.innerHeight*.95,
+                     window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 renderizador.render(escena,camara);
