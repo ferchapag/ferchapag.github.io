cc58ebddb298773b99c1651d2fee656642053a5c
ferchapag <fernando.chapa.g5@gmail.com>
2016-08-30 14:59:40 -0500

    Update and rename unionmalla.html to unionmalla.js

--- rv/unionmalla.js
+++ rv/unionmalla.js
@@ -12,3 +12,14 @@ arbolForma.merge(esferaMalla.geometry, esferaMalla.matrix);
 var material=new THREE.MeshNormalMaterial();
 var arbolMalla=new THREE.Mesh(arbolForma,material);
 
+var escena=new THREE.Scene();
+escena.add(arbolMalla);
+
+var camara=new THREE.PerspectiveCamera();
+camara.position.z=5;
+
+renderizador= new THREE.WebGlRenderer();
+renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena, camara);
+
