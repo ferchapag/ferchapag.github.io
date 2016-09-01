58b7665108874ef4563ab66a2fb44090a39c9740
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-01 15:00:39 -0500

    Update torre.js

--- rv/torre.js
+++ rv/torre.js
@@ -8,17 +8,16 @@ var pilarMalla=new THREE.Mesh(pilarForma);
 var torreForma= new THREE.Geometry();
 torreForma.merge(baseMalla.geometry, baseMalla.matrix);
 torreForma.merge(pilarMalla.geometry, pilarMalla.matrix);
+var torreMalla=new THREE.Mesh(torreForma);
 
 var material=new THREE.MeshNormalMaterial();
-var torreMalla=new THREE.Mesh(torreForma,material);
-
 var base2= new THREE.CylinderGeoemtry(1,1,1);
 base2.translate(0,3,0);
 var base2Malla=new THREE.Mesh(base2Forma);
 
 
 var torreForma1= new THREE.CylinGeometry(1,1,1);
-torreForma1.merge(torreForma.geometry, torreForma.matrix);
+torreForma1.merge(torreMalla.geometry, torreMalla.matrix);
 torreForma1.merge(base2Malla.geometry, base2Malla.matrix);
 
 var torreMalla1= new THREE.mesh(torreForma1, material);
