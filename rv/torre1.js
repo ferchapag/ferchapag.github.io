99cd3fb452601bd7aed6391284f6f14ee6017468
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-01 15:03:51 -0500

    Create torre1.js

---
+++ rv/torre1.js
@@ -0,0 +1,24 @@
+var baseForma= new THREE.CylinderGeometry(1,1,1);
+var pilarForma= new THREE.CylinderGeometry(.5,.5,2);
+pilarForma.translate(0,1,0);
+
+var baseMalla=new THREE.Mesh(baseForma);
+var pilarMalla=new THREE.Mesh(pilarForma);
+
+var torreForma= new THREE.Geometry();
+torreForma.merge(baseMalla.geometry, baseMalla.matrix);
+torreForma.merge(pilarMalla.geometry, pilarMalla.matrix);
+
+var material=new THREE.MeshNormalMaterial();
+var torreMalla=new THREE.Mesh(torreForma,material);
+
+var escena=new THREE.Scene();
+escena.add(torreMalla);
+
+var camara=new THREE.PerspectiveCamera();
+camara.position.z=5;
+
+renderizador= new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena, camara);
