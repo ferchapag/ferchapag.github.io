65937716e36d2ba323f7294321baad66e47e1260
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-01 15:34:48 -0500

    Update torre1.js

--- rv/torre1.js
+++ rv/torre1.js
@@ -1,25 +1,11 @@
 var baseForma= new THREE.CylinderGeometry(1,1,1);
 var pilarForma= new THREE.CylinderGeometry(.5,.5,2);
 pilarForma.translate(0,1,0);
-var puntos = [];
-for(var i = 0; i<50; i++){
-  puntos.push(new THREE.Vector2(
-                  Math.sin(i*0.2)*15+50,
-                  (i-5)*2));
-}
-var forma= new THREE.LatheGeometry(puntos);
-forma.translate(0,3,0);
 
 var baseMalla=new THREE.Mesh(baseForma);
 var pilarMalla=new THREE.Mesh(pilarForma);
-var revMalla=new THREE.Mesh(forma);
-
-
 
 var material=new THREE.MeshNormalMaterial();
-
-var malla= new THREE.Mesh(forma, material);
-malla.rotateX(Math.PI/6);
 
 var torreForma= new THREE.Geometry();
 torreForma.merge(baseMalla.geometry, baseMalla.matrix);
@@ -32,9 +18,6 @@ var torreMalla=new THREE.Mesh(torreForma,material);
 
 var escena=new THREE.Scene();
 escena.add(torreMalla);
-escena.add(malla);
-
-
 
 var camara=new THREE.PerspectiveCamera();
 camara.position.z=5;
