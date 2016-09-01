d4f35c0421394311e5216fe7b65bb7350514e747
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-01 15:42:30 -0500

    Update torre1.js

--- rv/torre1.js
+++ rv/torre1.js
@@ -24,6 +24,7 @@ escena.add(base2Malla);
 
 var camara=new THREE.PerspectiveCamera();
 camara.position.z=5;
+camara.position.y=10
 
 renderizador= new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
