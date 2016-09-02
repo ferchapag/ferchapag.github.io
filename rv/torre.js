c2596c146f55a0191c86e23fcc502ba0804ca54b
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-01 19:13:22 -0500

    Update torre.js

--- rv/torre.js
+++ rv/torre.js
@@ -1,35 +1,36 @@
-var baseForma= new THREE.CylinderGeometry(1,1,1);
-var pilarForma= new THREE.CylinderGeometry(.5,.5,2);
-pilarForma.translate(0,1,0);
+var base= new THREE.Shape();
+base.moveTo(10,10);
+base.lineTo(20,10);
+base.lineTo(20,20);
+base.lineTo(10,20);
+base.lineTo(10,10);
+var baseForma=new THREE.ExtrudeGeometry(base,{amount:10});
+var puntos = [];
+for(var i = 0; i<50; i++){
+  puntos.push(new THREE.Vector2(Math.sin(i*0.2)*1+10,(i)*2));
+}
+var material=new THREE.MeshNormalMaterial();
 
-var baseMalla=new THREE.Mesh(baseForma);
-var pilarMalla=new THREE.Mesh(pilarForma);
+var rev=new THREE.LatheGeometry(puntos);
+
+var revMalla= new THREE.Mesh(rev, material);
+var baseMalla=new THREE.Mesh(baseForma,material);
 
 var torreForma= new THREE.Geometry();
+torreForma.merge(revMalla.geometry, revMalla.matrix);
 torreForma.merge(baseMalla.geometry, baseMalla.matrix);
-torreForma.merge(pilarMalla.geometry, pilarMalla.matrix);
-var torreMalla=new THREE.Mesh(torreForma);
-
-var material=new THREE.MeshNormalMaterial();
-var base2= new THREE.CylinderGeoemtry(1,1,1);
-base2.translate(0,3,0);
-var base2Malla=new THREE.Mesh(base2Forma);
+var torreMalla=new THREE.Mesh(torreForma,material);
 
 
-var torreForma1= new THREE.CylinGeometry(1,1,1);
-torreForma1.merge(torreMalla.geometry, torreMalla.matrix);
-torreForma1.merge(base2Malla.geometry, base2Malla.matrix);
-
-var torreMalla1= new THREE.mesh(torreForma1, material);
 
 var escena=new THREE.Scene();
-escena.add(torreMalla1);
+escena.add(torreForma);
 
 var camara=new THREE.PerspectiveCamera();
-camara.position.z=5;
+camara.position.z=10;
+
 
 renderizador= new THREE.WebGLRenderer();
 renderizador.setSize(window.innerHeight*.95, window.innerHeight*.95);
 document.body.appendChild(renderizador.domElement);
 renderizador.render(escena, camara);
-
