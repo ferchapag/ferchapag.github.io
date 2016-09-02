e2809324612f5dc027830116bab5e94e8ca1b2a2
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-01 19:18:34 -0500

    Update torre.js

--- rv/torre.js
+++ rv/torre.js
@@ -1,10 +1,3 @@
-var base= new THREE.Shape();
-base.moveTo(10,10);
-base.lineTo(20,10);
-base.lineTo(20,20);
-base.lineTo(10,20);
-base.lineTo(10,10);
-var baseForma=new THREE.ExtrudeGeometry(base,{amount:10});
 var puntos = [];
 for(var i = 0; i<10; i++){
   puntos.push(new THREE.Vector2(Math.sin(i*0.2)*1+10,(i)*2));
@@ -12,6 +5,15 @@ for(var i = 0; i<10; i++){
 var material=new THREE.MeshNormalMaterial();
 
 var rev=new THREE.LatheGeometry(puntos);
+
+var base= new THREE.Shape();
+base.moveTo(10,10);
+base.lineTo(20,10);
+base.lineTo(20,20);
+base.lineTo(10,20);
+base.lineTo(10,10);
+var baseForma=new THREE.ExtrudeGeometry(base,{amount:10});
+
 
 var revMalla= new THREE.Mesh(rev, material);
 var baseMalla=new THREE.Mesh(baseForma,material);
