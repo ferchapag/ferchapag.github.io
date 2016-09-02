0c1cc00134660b78ada8080653ba6e1483d13c68
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-01 19:10:42 -0500

    Update torre1.js

--- rv/torre1.js
+++ rv/torre1.js
@@ -4,10 +4,10 @@ base.lineTo(20,10);
 base.lineTo(20,20);
 base.lineTo(10,20);
 base.lineTo(10,10);
-var baseForma=new THREE.ExtrudeGeometry(base,{amount:100});
+var baseForma=new THREE.ExtrudeGeometry(base,{amount:10});
 var puntos = [];
 for(var i = 0; i<50; i++){
-  puntos.push(new THREE.Vector2(Math.sin(i*0.2)*1+50,(i)*2));
+  puntos.push(new THREE.Vector2(Math.sin(i*0.2)*1+10,(i)*2));
 }
 var material=new THREE.MeshNormalMaterial();
 
