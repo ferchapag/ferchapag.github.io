0a46452b1337b380bbe5d0b9aa1dedaff07ca0b1
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-01 19:21:47 -0500

    Update torre.js

--- rv/torre.js
+++ rv/torre.js
@@ -1,5 +1,5 @@
 var puntos = [];
-for(var i = 0; i<75; i++){
+for(var i = 0; i<10; i++){
   puntos.push(new THREE.Vector2(Math.sin(i*0.2)*1+10,(i)*2));
 }
 var material=new THREE.MeshNormalMaterial();
@@ -7,11 +7,11 @@ var material=new THREE.MeshNormalMaterial();
 var rev=new THREE.LatheGeometry(puntos);
 
 var base= new THREE.Shape();
-base.moveTo(10,75);
-base.lineTo(20,75);
+base.moveTo(10,10);
+base.lineTo(20,10);
 base.lineTo(20,20);
 base.lineTo(10,20);
-base.lineTo(10,75);
+base.lineTo(10,10);
 var baseForma=new THREE.ExtrudeGeometry(base,{amount:10});
 
 
@@ -26,10 +26,10 @@ var torreMalla=new THREE.Mesh(torreForma,material);
 
 
 var escena=new THREE.Scene();
-escena.add(torreForma);
+escena.add(torreMalla);
 
 var camara=new THREE.PerspectiveCamera();
-camara.position.z=210;
+camara.position.z=20;
 
 
 renderizador= new THREE.WebGLRenderer();
