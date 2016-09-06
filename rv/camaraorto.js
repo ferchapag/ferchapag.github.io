cb2b10ae0217f25f327ddeebfa4cc9bad98c08a6
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-06 15:12:35 -0500

    Update camaraorto.js

--- rv/camaraorto.js
+++ rv/camaraorto.js
@@ -9,16 +9,16 @@ camara.far=1000;
 camara.updateProjectionMatrix();
 camara.position.z=100;
 
-var cubo=new THREE.Mesh(new THREE.BoxGeometry(2,2,2),new THREE.MeshNormalMaterial());
+var cubo=new THREE.Mesh(new THREE.BoxGeometry(30,30,30),new THREE.MeshNormalMaterial());
 
 cubo.rotateY(Math.PI/4);
 
-var esfera1=new THREE.Mesh(new THREE.SphereGeometry(1), new THREE.MeshNormalMaterial() );
-esfera1.position.x=5;
+var esfera1=new THREE.Mesh(new THREE.SphereGeometry(15), new THREE.MeshNormalMaterial() );
+esfera1.position.x=50;
 
-var esfera2=new THREE.Mesh(new THREE.SphereGeometry(1),new THREE.MeshNormalMaterial() );
-esfera2.position.x=-5;
-esfera2.position.z=-10;
+var esfera2=new THREE.Mesh(new THREE.SphereGeometry(15),new THREE.MeshNormalMaterial() );
+esfera2.position.x=-50;
+esfera2.position.z=-100;
 
 var escena=new THREE.Scene();
 escena.add(esfera1);
