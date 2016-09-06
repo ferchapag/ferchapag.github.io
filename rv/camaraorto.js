74c58cf10b8b54f589b065a891acbdcbb3a9d9f1
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-06 15:21:49 -0500

    Update camaraorto.js

--- rv/camaraorto.js
+++ rv/camaraorto.js
@@ -1,7 +1,7 @@
 var camara= new THREE.OrthographicCamera();
 
 camara.left=window.innerWidth/-2;
-camara.right=window.innerWidth/-2;
+camara.right=window.innerWidth/2;
 camara.top=window.innerHeight/2;
 camara.bottom=window.innerHeight/-2;
 camara.near=0.1;
