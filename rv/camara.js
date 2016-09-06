b3a8a59608ae938454bc17646d02b18401e01552
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-06 15:06:07 -0500

    Update camara.js

--- rv/camara.js
+++ rv/camara.js
@@ -1,7 +1,7 @@
 var campoVision=45;
 var relacionAspecto=window.innerWidth/window.innerHeight;
 var planoCercano=1;
-var planoleLejano=1000;
+var planoLejano=1000;
 
 var camara=new THREE.PerspectiveCamera(campoVision, relacionAspecto, planoCercano, planoLejano);
 
