8f7e7788844a284dfec8ae512d4f68f88424e5d9
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-08 15:43:23 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -1,9 +1,46 @@
+var blanco= new THREE.MeshBasicMaterial( {color: 0xFFFFFF} );
+var gris= new THREE.MeshBasicMaterial( {color: 0x6E6E6E} );
+
+var l=10;
 var tablero = [];
-for(var i = 0; i<=8; i++){
-  for(var j=0;j<=8;j++){
-  var cubo = new THREE.BoxGeometry( 10, 10, 10);
-  var blanco= new THREE.MeshBasicMaterial( {color: 0xFFFFFF} );
-  var negro= new THREE.MeshBasicMaterial( {color: 0x6E6E6E} );
-  var cube = new THREE.Mesh( geometry, material );
+
+for(var i=0;i<8;i++){
+  for(var j=0;j<8;j++){
+    var mat = gris;
+     if (i% 2){ 
+      if (j % 2){ 
+     mat=gris;
+      }
+      else{
+        mat=blanco;
+      }
+    }
+    else{
+      if (j % 2){ 
+      mat=gris;
+      }
+      else{
+        mat=blanco;
+      }
+    }
+    var cubo= new THREE.Mesh(new THREE.BoxGeometry(l,l,l),mat);
+    cubo.position.x=j*l;
+    cubo.position.y=i*l;
+    tablero.push(cubo);
+    }
+
   }
-}
+
+var escena= new THREE.Scene();
+escena.add(tablero);
+
+var camara= new THREE.PerspectiveCamera();
+camara.position.x=100;
+camara.position.y=100;
+camara.position.z=100;
+
+var renderizador= new THREE.WebGLRenderer();
+renderizador.setSize(window.innerHeight*.95,
+                     window.innerHeight*.95);
+document.body.appendChild(renderizador.domElement);
+renderizador.render(escena,camara);
