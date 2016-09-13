0bc833d685d86b1d414b2c626fd4b30ace169618
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-13 15:15:49 -0500

    Update lambert.js

--- rv/lambert.js
+++ rv/lambert.js
@@ -1,4 +1,4 @@
-var LuzPuntual=new THREE.PointLight(0xFFFFFF);
+var LuzPuntual=new THREE.PointLight(0xFF0000);
 LuzPuntual.position.set(10,10,10);
 var forma= new THREE.SphereGeometry(1);
 var material= new THREE.MeshLambertMaterial({color: '#00cc00'})
