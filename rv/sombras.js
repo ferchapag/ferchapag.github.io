890d45433883e02934ac1dbfa2463d7be3f4fe52
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-13 15:30:28 -0500

    Create sombras.js

---
+++ rv/sombras.js
@@ -0,0 +1,23 @@
+var iluminacion=new THREE.PointLight(0xFFFFFF);
+iluminacion.position.set(0,20,0);
+
+var forma= new THREE.SphereGeometry(1);
+var material= new THREE.MeshLambertMaterial({color: '#00cc00'})
+var malla= new THREE.Mesh(forma, material)
+mala.position.y=2;
+
+var base=new THREE.Mesh(new THREE.BoxGeometry(5,.1,5), new THREE.MeshLambertMAterial({color:0xFFFFFF}));
+
+var escena=new THREE.Scene();
+escena.add(malla);
+escena.add(iluminacion);
+escena.add(base);
+
+var camara = new THREE.PerspectiveCamera();
+camara.position.set(0,5,15);
+
+var lienzo= document.getElementById("luzsinSombras");
+var renderizador=new THREE.WebGLRenderer({canvas:lienzo, antialias:true});
+
+renderizador.setSize(600,600);
+renderizador.render(escena, camara);
