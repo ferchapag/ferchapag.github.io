e8d5101becd054e34af851a61ff30271f0574006
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-13 14:50:14 -0500

    Update and rename luz.js to basicmaterial.js

--- rv/basicmaterial.js
+++ rv/basicmaterial.js
@@ -14,5 +14,3 @@ var renderizador=new THREE.WebGLRenderer({canvas:lienzo, antialias:true});
 renderizador.setSize(600,600);
 renderizador.render(escena,camara);
 
-<canvas id="basicMaterial" width="600" height="600"></canvas>
-<script src="ejemplo-basicMaterial.js"></script>
