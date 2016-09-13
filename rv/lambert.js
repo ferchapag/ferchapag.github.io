5645ca29b71a404b35a4ecdab439e2071aeea9c9
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-13 15:06:30 -0500

    Update lambert.js

--- rv/lambert.js
+++ rv/lambert.js
@@ -16,5 +16,5 @@ camara.position.z=5;
 var lienzo= document.getElementById("lambertMaterial");
 var renderizador=new THREE.WebGLRenderer({canvas:lienzo, antialias:true});
 
-renderizador.SetSize(600,600);
+renderizador.setSize(600,600);
 renderizador.render(escena, camara);
