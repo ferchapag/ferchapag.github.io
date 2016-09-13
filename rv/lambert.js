80a03d5ae2159b233503c66fd82761fc154a6c6f
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-13 15:02:02 -0500

    Update lambert.js

--- rv/lambert.js
+++ rv/lambert.js
@@ -1,6 +1,6 @@
 var forma= new THREE.SphereGeometry(1);
 var material= new THREE.MeshLambertMaterial({color: '#00cc00'})
-var mala= new THREE.Mesh(forma, material)
+var malla= new THREE.Mesh(forma, material)
 
 var escena=new THREE.Scene();
 escena.add(malla);
