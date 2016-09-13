bade8251c355ab01c24eeb1828fa7b8c75b49b36
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-13 15:32:40 -0500

    Update sombras.js

--- rv/sombras.js
+++ rv/sombras.js
@@ -6,7 +6,7 @@ var material= new THREE.MeshLambertMaterial({color: '#00cc00'})
 var malla= new THREE.Mesh(forma, material)
 malla.position.y=2;
 
-var base=new THREE.Mesh(new THREE.BoxGeometry(5,.1,5), new THREE.MeshLambertMAterial({color:0xFFFFFF}));
+var base=new THREE.Mesh(new THREE.BoxGeometry(5,.1,5), new THREE.MeshLambertMaterial({color:0xFFFFFF}));
 
 var escena=new THREE.Scene();
 escena.add(malla);
