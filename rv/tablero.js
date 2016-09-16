73f67a0987714b3fa0e323b591da94240335441d
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-16 10:19:21 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -1,49 +1,59 @@
-var blanco= new THREE.MeshBasicMaterial( {color: 0xFFFFFF} );
-var gris= new THREE.MeshBasicMaterial( {color: 0x6E6E6E} );
+var colorGris = new THREE.Color("rgb(30, 30, 30)");
+var materialGris = new THREE.MeshBasicMaterial();
+materialGris.color = colorGris;
+var colorBlanco = new THREE.Color("rgb(255, 255, 255)");
+var materialBlanco = new THREE.MeshBasicMaterial();
+materialBlanco.color = colorBlanco;
 
-function par(n) {
+function isEven(n) {
    return n % 2 == 0;
 }
 
-function non(n) {
+function isOdd(n) {
    return Math.abs(n % 2) == 1;
 }
 
-var l=10;
-var tablero = [];
-for(var i=0;i<8;i++){
-  for(var j=0;j<8;j++){
-    var mat = gris;
-     if (par(i)){ 
-      if (non(j)){ 
-        mat=blanco;
-      }
-      }else{
-        if(par(j)){
-          mat=blanco;
-        }
-      }
-    var cu= new THREE.Mesh(new THREE.BoxGeometry(l,l,l));
-    var cubo=new THREE.Mesh (cu,mat);
-    cubo.position.x=j*l;
-    cubo.position.y=i*l;
-    tablero.push(cubo);
+var cuadros = [];
+var cubeSize = 10;
+for (i = 0; i < 8; i++) {
+  for (j = 0; j < 8; j++) {
+    var material = materialGris;
+    if (isEven(i)) {
+      if (isOdd(j)) {
+        material = materialBlanco;
+      } 
+    } else {
+      if (isEven(j)) {
+        material = materialBlanco;
+      } 
     }
-
+    var geometry = new THREE.BoxGeometry( 10, 10, 10 );
+    var cubo = new THREE.Mesh(geometry,material);
+    cubo.position.x = j*cubeSize;
+    cubo.position.y = i*cubeSize; 
+    cuadros.push(cubo);
   }
-for (i = 1; i < 64; i++) {
-tablero[0].add(tablero[i]);
 }
-var escena= new THREE.Scene();
-escena.add(tablero);
+// Join cuadros
+for (i = 1; i < 64; i++) {
+cuadros[0].add(cuadros[i]);
+}
+// var cuboS = new THREE.Mesh(new THREE.BoxGeometry(cubeSize,cubeSize,cubeSize),material);
+//////////////////////////////////////////////////////////////////
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 100;
+camara.position.x = 55;
+camara.position.y = -55;
+camara.lookAt(new THREE.Vector3(35,35,11))
 
-var camara= new THREE.PerspectiveCamera();
-camara.position.x=10;
-camara.position.y=10;
-camara.position.z=10;
+var escena = new THREE.Scene();
+for (i = 0; i < 64; i++) {
+escena.add(cuadros[i]);
+}
 
-var renderizador= new THREE.WebGLRenderer();
-renderizador.setSize(window.innerHeight*.95,
-                     window.innerHeight*.95);
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(window.innerWidth,window.innerHeight);
+
 document.body.appendChild(renderizador.domElement);
 renderizador.render(escena,camara);
+
