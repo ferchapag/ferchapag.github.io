75f27b077c044274cd5050802f51cdfa513bcfd9
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-16 13:06:20 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -90,10 +90,10 @@ var camara = new THREE.PerspectiveCamera();
 camara.position.set(40,-100,100);
 camara.lookAt(new THREE.Vector3(40,40,12))
 
-var malla=new THREE.Mesh(new THREE.BoxGeometry(5,100,10), Marco);
+var malla=new THREE.Mesh(new THREE.BoxGeometry(5,90,10), Marco);
 malla.position.set(-7.5,35,0);
-var malla2=new THREE.Mesh(new THREE.BoxGeometry(5,100,10), Marco);
-malla2.position.set(75,35,0);
+var malla2=new THREE.Mesh(new THREE.BoxGeometry(5,90,10), Marco);
+malla2.position.set(80,35,0);
 var malla3=new THREE.Mesh(new THREE.BoxGeometry(80,5,10), Marco);
 malla3.position.set(35,75,0);
 var malla4=new THREE.Mesh(new THREE.BoxGeometry(80,5,10), Marco);
