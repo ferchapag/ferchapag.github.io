e00d255966abd8f9b23154ef5f079db103c530e9
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-16 12:36:41 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -1,3 +1,4 @@
+
 var Gris = new THREE.MeshBasicMaterial({color: 0xB0A9A7});
 var Blanco = new THREE.MeshBasicMaterial({color: 0xffffff});
 
@@ -12,18 +13,18 @@ function isOdd(n) {
 var tablero = [];
 for (i = 0; i < 8; i++) {
   for (j = 0; j < 8; j++) {
-    var material = Gris;
+    var mat = Gris;
     if (isEven(i)) {
       if (isOdd(j)) {
-        material = Blanco;
+        mat = Blanco;
       } 
     } else {
       if (isEven(j)) {
-        material = Blanco;
+        mat = Blanco;
       } 
     }
     var geometry = new THREE.BoxGeometry( 10, 10, 10 );
-    var cubo = new THREE.Mesh(geometry,material);
+    var cubo = new THREE.Mesh(geometry,mat);
     cubo.position.x = j*10;
     cubo.position.y = i*10; 
     tablero.push(cubo);
@@ -33,7 +34,57 @@ for (i = 0; i < 8; i++) {
 for (i = 1; i < 64; i++) {
 tablero[0].add(tablero[i]);
 }
+//Torre
+var base= new THREE.Shape();
+base.moveTo(-7,-4);
+base.lineTo(-7,0);
+base.lineTo(7,0);
+base.lineTo(7,-4);
+base.lineTo(-7,-4);
+var baseForma=new THREE.ExtrudeGeometry(base,{amount:10});
+var puntos = [];
+for(var i = 0; i<18; i++){
+  puntos.push(new THREE.Vector2(Math.sin(i*0.2)*1+7,(i)));
+}
+var material=new THREE.MeshBasicMaterial( {color: 0x6e6e6e});
 
+var rev=new THREE.LatheGeometry(puntos);
+
+var revMalla= new THREE.Mesh(rev, material);
+var baseMalla=new THREE.Mesh(baseForma,material);
+
+var pilarForma= new THREE.CylinderGeometry(10,10,5);
+pilarForma.translate(0,18,0);
+var base2Forma=new THREE.CylinderGeometry(4,4,5);
+base2Forma.translate(0,23,0);
+var base3Forma=new THREE.CylinderGeometry(10,10,5);
+base3Forma.translate(0,28,0);
+
+var pilarMalla=new THREE.Mesh(pilarForma,material);
+var base2Malla=new THREE.Mesh(base2Forma,material);
+var base3Malla=new THREE.Mesh(base3Forma,material);
+
+var torreForma= new THREE.Geometry();
+torreForma.merge(revMalla.geometry, revMalla.matrix);
+torreForma.merge(baseMalla.geometry, baseMalla.matrix);
+var torreMalla=new THREE.Mesh(torreForma,material);
+
+var torreForma1= new THREE.Geometry();
+torreForma1.merge(pilarMalla.geometry, pilarMalla.matrix);
+torreForma1.merge(torreMalla.geometry, torreMalla.matrix);
+var torre1Malla=new THREE.Mesh(torreForma1,material);
+
+var torreForma2= new THREE.Geometry();
+torreForma2.merge(torre1Malla.geometry, torre1Malla.matrix);
+torreForma2.merge(base2Malla.geometry, base2Malla.matrix);
+var torre2Malla=new THREE.Mesh(torreForma2,material);
+
+var torreForma3= new THREE.Geometry();
+torreForma3.merge(torre2Malla.geometry, torre1Malla.matrix);
+torreForma3.merge(base3Malla.geometry, base3Malla.matrix);
+var torre3Malla=new THREE.Mesh(torreForma3,material);
+
+//camara
 var camara = new THREE.PerspectiveCamera();
 camara.position.set(40,-100,100);
 camara.lookAt(new THREE.Vector3(40,40,12))
@@ -46,7 +97,9 @@ camara.lookAt(new THREE.Vector3(40,40,12))
 var escena = new THREE.Scene();
 for (i = 0; i < 64; i++) {
 escena.add(tablero[i]);
+escena.add(torre3Malla);
 }
+
 
 
 var renderizador = new THREE.WebGLRenderer();
