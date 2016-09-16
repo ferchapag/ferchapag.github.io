2836be710347da7f0cf3a529566c7d4f6e783e06
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-16 13:26:08 -0500

    Update tablero.js

--- rv/tablero.js
+++ rv/tablero.js
@@ -85,7 +85,9 @@ torreForma3.merge(torre2Malla.geometry, torre1Malla.matrix);
 torreForma3.merge(base3Malla.geometry, base3Malla.matrix);
 var torre3Malla=new THREE.Mesh(torreForma3,material);
 torre3Malla.rotateZ(-1.57);
-torre3Malla.position.set(0,20,0);
+torre3Malla.rotateX(-1.57);
+
+torre3Malla.position.set(0,0,10);
 
 //camara
 var camara = new THREE.PerspectiveCamera();
