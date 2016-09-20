4424015cf5ec5144ce9f29d808b8c20f13fc09b1
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-20 15:27:30 -0500

    Update tablerosombra.js

--- rv/tablerosombra.js
+++ rv/tablerosombra.js
@@ -1,9 +1,9 @@
 var iluminacion=new THREE.PointLight(0x00FFFF);
-iluminacion.position.set(35,-2,32);
+iluminacion.position.set(35,-10,20);
 var iluminacion1=new THREE.PointLight(0xFF00FF);
-iluminacion1.position.set(0,70,32);
+iluminacion1.position.set(-10,80,20);
 var iluminacion2=new THREE.PointLight(0xFFFF00);
-iluminacion2.position.set(70,70,32);
+iluminacion2.position.set(80,-10,20);
 
 var Gris = new THREE.MeshLambertMaterial({color: 0xB0A9A7});
 var Blanco = new THREE.MeshLambertMaterial({color: 0xffffff});
