a04bce9bee232d164321be6ba017439222d94a77
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-22 15:05:17 -0500

    Update funciones.js

--- rv/funciones.js
+++ rv/funciones.js
@@ -1,16 +1,19 @@
+var escena, camara, renderizador;
+init(1);
+
 function init(p) {
 
 var material = new THREE.MeshNormalMaterial();
 var malla = new THREE.Mesh( new THREE.BoxGeometry(p,p,p), material);
 
-var escena = new THREE.Scene();
+escena = new THREE.Scene();
 escena.add(malla);
 
-var camara = new THREE.PerspectiveCamera();
+camara = new THREE.PerspectiveCamera();
 camara.position.z = 3*p;
 
-var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95);
+renderizador = new THREE.WebGLRenderer();
+renderizador.setSize(100,700);
 document.body.appendChild(renderizador.domElement);
 renderizador.render(escena,camara);
 
