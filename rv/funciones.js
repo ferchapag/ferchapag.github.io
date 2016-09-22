5111b4e5a0140107c888862823e491a2dc503e7b
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-22 15:24:02 -0500

    Update funciones.js

--- rv/funciones.js
+++ rv/funciones.js
@@ -1,9 +1,9 @@
-var escena, camara, renderizador;
+var escena, camara, renderizador,malla,material;
 
 function init(p) {
 
-var material = new THREE.MeshNormalMaterial();
-var malla = new THREE.Mesh( new THREE.BoxGeometry(p,p,p), material);
+material = new THREE.MeshNormalMaterial();
+malla = new THREE.Mesh( new THREE.BoxGeometry(p,p,p), material);
 
 escena = new THREE.Scene();
 escena.add(malla);
@@ -16,9 +16,11 @@ renderizador.setSize(100,700);
 document.body.appendChild(renderizador.domElement);
 
 }
- var main = function(p) {
-   p(1);
+ var loop = function(p) {
+   requestAnimationFrame(loop);
    renderizador.render(escena,camara);
+   malla.rotateY(.01);
  }
  
  main(init);
+ loop();
