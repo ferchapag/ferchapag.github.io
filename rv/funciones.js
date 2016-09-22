8c6c451efb99d80facc4a3a096018c0e983e2401
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-22 15:34:38 -0500

    Update funciones.js

--- rv/funciones.js
+++ rv/funciones.js
@@ -17,7 +17,10 @@ document.body.appendChild(renderizador.domElement);
  var loop = function(p) {
    requestAnimationFrame(loop);
    renderizador.render(escena,camara);
-   malla.rotateY(.01);
+   malla.Position.x(+1);
+   if(malla.Position.x==700){
+    malla.Position.x=700-1
+   }
  }
  var escena, camara, renderizador,malla,material;
  init(1);
