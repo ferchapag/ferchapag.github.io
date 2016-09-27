108e4bd2d69d2ebf98a3e3d9ac42305ef769ecea
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-27 15:24:36 -0500

    Update mod.js

--- rv/mod.js
+++ rv/mod.js
@@ -9,7 +9,7 @@ VENTANA.listener=function(){
 VENTANA.setup=function(){
   var tipo_evento='resize';
   var capturarp=false;
-  window.addEventListener(tipo_evento,ventana.listener,capturarp);
+  window.addEventListener(tipo_evento,VENTANA.listener,capturarp);
   VENTANA.escena=new THREE.Scene();
   VENTANA.camara=new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1,1000);
   VENTANA.camara.position.z=5;
@@ -19,14 +19,14 @@ VENTANA.setup=function(){
   VENTANA.renderizador.setSize(window.innerWidth, window.innerHeight);
   
   VENTANA.malla=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
-  VENTANA.escena.add(ventana.malla)
+  VENTANA.escena.add(VENTANA.malla)
  }
  
  VENTANA.loop=function(){
   requestAnimationFrame(VENTANA.loop);
   VENTANA.malla.rotateX(0.01);
   VENTANA.malla.rotateY(0.01);
-  VENTANA.renderizador.render(ventana.escena, ventana.camara);
+  VENTANA.renderizador.render(VENTANA.escena, VENTANA.camara);
  }
  
  VENTANA.setup();
