1b0bf65866327fb06abca306cc26cc989334d8f2
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-27 15:30:44 -0500

    Update textura.js

--- rv/textura.js
+++ rv/textura.js
@@ -6,8 +6,20 @@ TEXTURA.retrollamada=function(textura){
    TEXTURA.esscena.add(TEXTURA.malla);
   }
   
-  TEXTURA.escena=new THREE.Scene();
-  var cargador=new TREE.TextureLoader();
-  
-  cargador.load("earth.jpg", TEXTURA.retrollamada);
-  TEXTURA.camara=new THREE.PerspectiveCamera
+ TEXTURA.setup=function(){
+    TEXTURA=new THREE.Scene();
+    var cargador=new TREE.TextureLoader();
+    cargador.load("earth.jpg", TEXTURA.retrollamada);
+    TEXTURA.camara=new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,
+                                               0.1,
+                                               1000);
+   TEXTURA.camara.position.z=5;
+    
+    var lienzo=document.getElemenById("ejemplo-textura");
+    TEXTURA.renderizador=new THREE.WebGLRenderer({canvas: lienzo,
+                                                 antialias: true});
+    TEXTURA.renderizador.setSize(600,600);
+    
+    TEXTURA.loop=function(){
+      requestAnimationFrame(TEXTURA.loop);
+      
