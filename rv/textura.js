daddb0274a0c381076438e6515123d7f92b91a93
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-27 15:50:30 -0500

    Update textura.js

--- rv/textura.js
+++ rv/textura.js
@@ -3,13 +3,14 @@ var TEXTURA =new Object();
 TEXTURA.retrollamada=function(textura){
    var material=new THREE.MeshBasicMaterial({map: textura});
    TEXTURA.malla=new THREE.Mesh(new THREE.SphereGeometry(1), material);
-   TEXTURA.esscena.add(TEXTURA.malla);
+   TEXTURA.escena.add(TEXTURA.malla);
   }
   
 TEXTURA.setup=function(){
    TEXTURA=new THREE.Scene();
    var cargador=new THREE.TextureLoader();
-   cargador.load("earth.jpg", TEXTURA.retrollamada);
+   cargador.load("earth.jpg",
+                 TEXTURA.retrollamada);
    TEXTURA.camara=new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,
                                                0.1,
                                                1000);
