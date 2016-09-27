e9e8a0608f4cabe3d8a5e0a22fc00dfb0ef4aff9
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-27 15:34:44 -0500

    Update textura.js

--- rv/textura.js
+++ rv/textura.js
@@ -7,25 +7,25 @@ TEXTURA.retrollamada=function(textura){
   }
   
  TEXTURA.setup=function(){
-    TEXTURA=new THREE.Scene();
-    var cargador=new TREE.TextureLoader();
-    cargador.load("earth.jpg", TEXTURA.retrollamada);
-    TEXTURA.camara=new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,
+   TEXTURA=new THREE.Scene();
+   var cargador=new TREE.TextureLoader();
+   cargador.load("earth.jpg", TEXTURA.retrollamada);
+   TEXTURA.camara=new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,
                                                0.1,
                                                1000);
    TEXTURA.camara.position.z=5;
     
-    var lienzo=document.getElemenById("ejemplo-textura");
-    TEXTURA.renderizador=new THREE.WebGLRenderer({canvas: lienzo,
+   var lienzo=document.getElemenById("ejemplo-textura");
+   TEXTURA.renderizador=new THREE.WebGLRenderer({canvas: lienzo,
                                                  antialias: true});
-    TEXTURA.renderizador.setSize(600,600);
+   TEXTURA.renderizador.setSize(600,600);
     
-    TEXTURA.loop=function(){
-      requestAnimationFrame(TEXTURA.loop);
-       if(TEXTURA.malla!==undefined){
-          TEXTURA.malla.rotateX(.01);
-          TEXTURA.malla.rotateY(.01);
-       }
+TEXTURA.loop=function(){
+   requestAnimationFrame(TEXTURA.loop);
+   if(TEXTURA.malla!==undefined){
+     TEXTURA.malla.rotateX(.01);
+     TEXTURA.malla.rotateY(.01);
+   }
           
           
       
