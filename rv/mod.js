96d15b0dc11d2b01bcf9bbc92177266c8941f4ad
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-27 15:23:13 -0500

    Update mod.js

--- rv/mod.js
+++ rv/mod.js
@@ -1,36 +1,36 @@
-var ventana=new Object();
+var VENTANA=new Object();
 
-ventana.listener=function(){
-  ventana.camara.aspect=window.innerWidth/window.innerHeight;
-  ventana.camara.updateProjectionMatrix();
-  ventana.renderizador.setSize(window.innerWidth, window.innerHeight);
+VENTANA.listener=function(){
+  VENTANA.camara.aspect=window.innerWidth/window.innerHeight;
+  VENTANA.camara.updateProjectionMatrix();
+  VENTANA.renderizador.setSize(window.innerWidth, window.innerHeight);
  }
  
-ventana.setup=function(){
+VENTANA.setup=function(){
   var tipo_evento='resize';
   var capturarp=false;
   window.addEventListener(tipo_evento,ventana.listener,capturarp);
-  ventana.escena=new THREE.Scene();
-  ventana.camara=new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1,1000);
-  ventana.camara.position.z=5;
+  VENTANA.escena=new THREE.Scene();
+  VENTANA.camara=new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1,1000);
+  VENTANA.camara.position.z=5;
   
   var lienzo=document.getElementById("Ejemplo-ventana");
-  ventana.renderizador=new THREE.WebGLRenderer({canvas:lienzo, antialias: true});
-  ventana.renderizador.setSize(window.innerWidth, window.innerHeight);
+  VENTANA.renderizador=new THREE.WebGLRenderer({canvas:lienzo, antialias: true});
+  VENTANA.renderizador.setSize(window.innerWidth, window.innerHeight);
   
-  ventana.malla=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
-  ventana.escena.add(ventana.malla)
+  VENTANA.malla=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
+  VENTANA.escena.add(ventana.malla)
  }
  
- ventana.loop=function(){
-  requestAnimationFrame(ventana.loop);
-  ventana.malla.rotateX(0.01);
-  ventana.malla.rotateY(0.01);
-  ventana.renderizador.render(ventana.escena, ventana.camara);
+ VENTANA.loop=function(){
+  requestAnimationFrame(VENTANA.loop);
+  VENTANA.malla.rotateX(0.01);
+  VENTANA.malla.rotateY(0.01);
+  VENTANA.renderizador.render(ventana.escena, ventana.camara);
  }
  
- ventana.setup();
- ventana.loop();
+ VENTANA.setup();
+ VENTANA.loop();
  
 
  
