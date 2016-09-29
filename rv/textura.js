7b5b5ec68af2a270e336ba76f255ea6484e0f38b
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-29 15:44:25 -0500

    Update textura.js

--- rv/textura.js
+++ rv/textura.js
@@ -1,8 +1,47 @@
 var TEXTURA =new Object();
 
+
 TEXTURA.retrollamada=function(textura){
    var material=new THREE.MeshBasicMaterial({map: textura});
-   TEXTURA.malla=new THREE.Mesh(new THREE.SphereGeometry(1), material);
+   //Torre
+var base= new THREE.Shape();
+base.moveTo(-7,-4);
+base.lineTo(-7,4);
+base.lineTo(7,4);
+base.lineTo(7,-4);
+base.lineTo(-7,-4);
+var baseForma=new THREE.ExtrudeGeometry(base,{amount:10});
+var puntos = [];
+for(var i = 0; i<18; i++){
+  puntos.push(new THREE.Vector2(Math.sin(i*0.2)*1+7,(i)));
+}
+
+var rev=new THREE.LatheGeometry(puntos);
+
+var revMalla= new THREE.Mesh(rev, material);
+var baseMalla=new THREE.Mesh(baseForma,material);
+
+var pilarForma= new THREE.CylinderGeometry(10,10,5);
+pilarForma.translate(0,18,0);
+var base2Forma=new THREE.CylinderGeometry(4,4,5);
+base2Forma.translate(0,23,0);
+var base3Forma=new THREE.CylinderGeometry(10,10,5);
+base3Forma.translate(0,28,0);
+
+var pilarMalla=new THREE.Mesh(pilarForma,material);
+var base2Malla=new THREE.Mesh(base2Forma,material);
+var base3Malla=new THREE.Mesh(base3Forma,material);
+
+
+
+
+var torreForma= new THREE.Geometry();
+torreForma.merge(revMalla.geometry, revMalla.matrix);
+torreForma.merge(baseMalla.geometry, baseMalla.matrix);
+//var torreMalla=new THREE.Mesh(torreForma,material);
+   
+   
+   TEXTURA.malla=new THREE.Mesh(new THREE.torreForma, material);
    TEXTURA.escena.add(TEXTURA.malla);
   }
   
