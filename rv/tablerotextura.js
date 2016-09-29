d029245a08446f256b2cc0554e41912b851fa544
ferchapag <fernando.chapa.g5@gmail.com>
2016-09-29 15:09:12 -0500

    Update tablerotextura.js

--- rv/tablerotextura.js
+++ rv/tablerotextura.js
@@ -2,6 +2,42 @@ var Gris = new THREE.MeshBasicMaterial({color: 0xB0A9A7});
 var Blanco = new THREE.MeshBasicMaterial({color: 0xffffff});
 var Marco = new THREE.MeshBasicMaterial({color: 0xB59402});
 
+var TEXTURA =new Object();
+
+TEXTURA.retrollamada=function(textura){
+   var material=new THREE.MeshBasicMaterial({map: textura});
+   TEXTURA.malla=new THREE.Mesh(new THREE.SphereGeometry(1), material);
+   TEXTURA.escena.add(TEXTURA.malla);
+  }
+  
+TEXTURA.setup=function(){
+   TEXTURA.escena=new THREE.Scene();
+   var cargador=new THREE.TextureLoader();
+   cargador.load("earth.jpg",
+                 TEXTURA.retrollamada);
+   TEXTURA.camara=new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,
+                                               0.1,
+                                               1000);
+   TEXTURA.camara.position.z=5;
+    
+   var lienzo=document.getElementById("ejemplo-textura");
+   TEXTURA.renderizador=new THREE.WebGLRenderer({canvas: lienzo,
+                                                 antialias: true});
+   TEXTURA.renderizador.setSize(600,600);
+}
+    
+TEXTURA.loop=function(){
+   requestAnimationFrame(TEXTURA.loop);
+   if(TEXTURA.malla!==undefined){
+     TEXTURA.malla.rotateX(.01);
+     TEXTURA.malla.rotateY(.01);
+   }
+   TEXTURA.renderizador.render(TEXTURA.escena, TEXTURA.camara);
+   
+}
+TEXTURA.setup();
+TEXTURA.loop();
+
 function isEven(n) {
    return n % 2 == 0;
 }
