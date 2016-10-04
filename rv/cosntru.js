b7b0593a94ba52d41f79e0c7bf31445c6c1b71a3
ferchapag <fernando.chapa.g5@gmail.com>
2016-10-04 15:07:46 -0500

    Update cosntru.js

--- rv/cosntru.js
+++ rv/cosntru.js
@@ -6,9 +6,29 @@ function Arbol(){
   var troncoMalla=new THREE.Mesh(troncoForma);
   var esferaMalla=new THREE.Mesh(esferaForma);
  
- var arbolForma= new THREE.Geometry();
- arbol.Forma=new
+  var arbolForma= new THREE.Geometry();
+  arbolForma.merge(troncoMalla.geometry, troncoMalla.matrix);
+  arbolForma.merge(esferaMalla.geometry, esferaMalla.matrix);
+  
   var material=new THREE.MeshNormalMaterial();
+  this.malla=new THREE.Mesh(arbolForma, material);
  }
  
- 
+var CONSTRUCTOR=new Object();
+
+CONSTRUCTOR.setup=function(){
+  var arbol1=new Arbol();
+  var arbol2=new Arbol();
+  
+  arbol1.malla.position.x=-5;
+  arbol2.malla.position.x=5;
+  
+  CONSTRUCTOR.camara=new THREE.PerspectiveCamera();
+  CONSTRUCTOR.camara.position.z=20;
+  
+  var lienzo=document.getElementById("ejemplo-csontructor");
+  CONSTRUCTOR.renderizador=new THREE.WebGLRenderer({canvas:lienzo, antialias:true});
+  CONSTRUCTOR.renderizador.setSize(600,600);
+}
+
+  
