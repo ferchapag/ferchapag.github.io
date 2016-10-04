fbd0ae9eb4eb97ff028b500f4f44c4be15705a14
ferchapag <fernando.chapa.g5@gmail.com>
2016-10-04 14:59:20 -0500

    Create cosntru.js

---
+++ rv/cosntru.js
@@ -0,0 +1,14 @@
+function Arbol(){
+  var troncoForma=new THREE.CylinderGeometry(.25,.5,1);
+  var esferaForma=new THREE.SphereGeometry(.65);
+  esferaForma.translate(0,1,0);
+  
+  var troncoMalla=new THREE.Mesh(troncoForma);
+  var esferaMalla=new THREE.Mesh(esferaForma);
+ 
+ var arbolForma= new THREE.Geometry();
+ arbol.Forma=new
+  var material=new THREE.MeshNormalMaterial();
+ }
+ 
+ 
