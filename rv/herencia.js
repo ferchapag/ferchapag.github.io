0f5dbbe0455a3e7dc095e41790e4378bbc574aac
ferchapag <fernando.chapa.g5@gmail.com>
2016-10-04 15:56:21 -0500

    Update herencia.js

--- rv/herencia.js
+++ rv/herencia.js
@@ -3,6 +3,7 @@ ArbolGeometry=function(){
 
   var troncoForma=new THREE.CylinderGeometry(.25,.5,1);
   var esferaForma=new THREE.SphereGeometry(.65);
+  esferaForma.translate(0,1,0);
 
   var troncoMalla=new THREE.Mesh(troncoForma);
   var esferaMalla=new THREE.Mesh(esferaForma);
