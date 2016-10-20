73acdac4de4a2358a149a94cf3d2e7b3a4ba1e33
ferchapag <fernando.chapa.g5@gmail.com>
2016-10-20 15:30:09 -0500

    Update obmovil.js

--- rv/obmovil.js
+++ rv/obmovil.js
@@ -4,12 +4,12 @@ THREE.Object3D.call(this);
 this.piernaizq=new THREE.Mesh(new THREE.BoxGeometry(1,5,1));
 this.piernader=new THREE.Mesh(new THREE.BoxGeometry(1,5,1));
 var cuerpo=new THREE.Mesh(new THREE.BoxGeometry(5,10,5));
-this.add(this.piernaizq, this.piernader, this.cuerpo);
+this.add(this.piernaizq, this.piernader, cuerpo);
 this.piernaizq.position.z=-2;
 this.piernaizq.position.y=-2.5;
 this.piernader.position.z=2;
 this.piernader.position.y=-2.5;
-this.cuerpo.position.z=2.5;
+cuerpo.position.z=2.5;
 }
 var pieza
 Pieza.prototype=new THREE.Object3D
