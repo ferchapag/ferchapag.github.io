6c6b661ae2a427d78f0d9686072ac361c98fb108
ferchapag <fernando.chapa.g5@gmail.com>
2016-10-25 14:55:34 -0500

    Update agente.js

--- rv/agente.js
+++ rv/agente.js
@@ -6,31 +6,35 @@ function Agent(x=0, y=0){
  
 Agent.prototype=new THREE.Object3D();
 
-Agent.prototype.sense=function(evironment){};
-Agent.prototype.plan=function(environment){};
-Agent.prototype.act= function(environment){};
+Agent.prototype.sense=function(evironment) {};
+Agent.prototype.plan=function(environment) {};
+Agent.prototype.act= function(environment) {};
+
+function Environment(){
+  Three.Scene.call(this);
+}
 
 Environment.prototype=new THREE.Scene();
 
 Environment.prototype.sense=function(){
-  for (var i=0; i<this.children.length;i++){
+  for (var i=0; i < this.children.length; i++){
     if(this.children[i].sense !==undefined)
       this.children[i].sense(this);
-}
+  }
 }
 
 Environment.prototype.plan=function(){
-  for (var i=0; i<this.children.length;i++){
+  for (var i=0; i < this.children.length; i++){
     if(this.children[i].plan !==undefined)
       this.children[i].plan(this);
-}
+  }
 }
 
 Environment.prototype.act=function(){
-  for (var i=0; i<this.children.length;i++){
+  for (var i=0; i < this.children.length; i++){
     if(this.children[i].act !==undefined)
       this.children[i].act(this);
-}
+  }
 }
  
  
